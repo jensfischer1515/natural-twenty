428cd4d2ddef5ba85e707f86bea124af8df0c540
Jens Fischer <soth@gmx.net>
2017-10-12 23:02:17 +0200

    first bootstrap buttons
    and profile-dependent manifest for caching

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ plus the desire to try out modern technologies like Kotlin.
 * http://drjon.typepad.com/.a/6a00d83451aec269e201b8d29e4301970c-pi
 * https://i.pinimg.com/originals/cd/cf/ac/cdcfac637a8b774b9a60b696bcdb8a86.jpg
 * http://iconifier.net/
+
+
+### Manage app cache on Chrome
+
+[chrome://appcache-internals/#](chrome://appcache-internals/#)
--- src/main/kotlin/org/openended/pfcoca/WebConfiguration.kt
+++ src/main/kotlin/org/openended/pfcoca/WebConfiguration.kt
@@ -1,11 +1,17 @@
 package org.openended.pfcoca
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfiguration : WebMvcConfigurer {
+
+    @Bean
+    fun useCache(env: Environment) = env.activeProfiles.contains("cache")
+
     override fun addViewControllers(registry: ViewControllerRegistry) {
         registry.addViewController("/").setViewName("index")
     }
